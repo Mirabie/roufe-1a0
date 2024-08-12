e46eaf105a55de9ecbf2f73a5dd00c3344538c6f
Mirabie <mirmdshabie@gmail.com>
2024-08-12 22:52:41 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('clickButton');
+    button.addEventListener('click', () => {
+        alert('Button Clicked!');
+    });
+    window.location.href = "https://www.highratecpm.com/ptjswnwxw?key=b1760aedcfc1d2f79985b210f95691a0";
+});
